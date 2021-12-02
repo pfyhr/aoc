d0a1913220db65f55c73abe892573d19ddf1ba6f
pf831 <pontus.fyhr@haldex.com>
2021-12-02 14:06:02 +0100

    one program to solve them all!

--- day2/c/position.c
+++ day2/c/position.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int readFile(char *filename)
+int readFile(char *filename, int problem)
 {
     static int retval = 1;
     static int stepl;
@@ -29,15 +29,32 @@ int readFile(char *filename)
         if (direction[0] == 102) /* forward */
         {
             xpos += stepl;
-            ypos += aim * stepl;
+            if (problem ==2)
+            {
+                ypos += aim * stepl;
+            }
         }
         else if (direction[0] == 117) /* up aim */
         {
-            aim -= stepl;
+            if (problem == 2)
+            {
+                aim -= stepl;
+            }
+            else
+            {
+                ypos -= stepl;
+            }
         }
         else if (direction[0] == 100) /* down aim */
         {
-            aim += stepl;
+            if ( problem == 2)
+            {
+                aim += stepl;
+            }
+            else
+            {
+                ypos += stepl;
+            }
         }
         else
         {
@@ -56,18 +73,36 @@ int readFile(char *filename)
 int main(int argc, char *argv[])
 {
     static int retval = 1;
+    static int problem;
 
-    if (argc > 2)
+    if (argc > 3)
     {
-        printf("Too many arguments, usage is: ./depth path/to/file.txt n\n");
+        printf("Too many arguments, usage is: ./position path/to/file.txt n\n");
+    }
+    else if (argc > 2)
+    {
+
+        retval = sscanf (argv[2], "%d", &problem);
+        if (retval == 1 && (problem == 1 || problem == 2))
+        {
+            retval = readFile(argv[1], problem);
+        }
+        else
+        {
+            printf("second input n, must be either 1 or 2\n");
+            return 0;
+        }
     }
     else if (argc > 1)
     {
-        retval = readFile(argv[1]);
+        printf("Too few arguments, usage is: ./position path/to/file.txt n\n");
+        printf("Defaulting to problem 2\n");
+        problem = 2;
+        retval = readFile(argv[1], problem);
     }
     else
     {
-        printf("no input file provided, usage is: ./depth path/to/file.txt n\n");
+        printf("no input file provided, usage is: ./position path/to/file.txt n\n");
     }
 
     return retval;
