109284f43fa58c78f952551b185541d30f3bd4b2
pf831 <pontus.fyhr@haldex.com>
2021-12-05 12:57:39 +0100

    a bit more on the way, moving to a different computer

--- day4/bingo.py
+++ day4/bingo.py
@@ -11,17 +11,30 @@ def checkrows(board):
             counter = 0
     return 'nothing'
 
+def marknumber(number, board, marks):
+    for ridx, row in enumerate(board):
+        for cidx, item in enumerate(row):
+            if item == number:
+                marks[ridx][cidx] = 1
+    return marks
+
+
+
 with open('day4/input.txt') as f:
     #lines = f.readlines()
     lines = [line.rstrip() for line in f]
 
 input = lines[0]
 #print(input)
+# fake input here
+input = [50, 98, 65, 14, 47]
 
 skip = 2
 rows = 5
 board = [ ] #empty list of lists for board
+marks = [ ]
 boards = []    #list to hold boards
+markboards = []
 
 #boards.append(board)
 #boards.append(board)
@@ -41,12 +54,24 @@ for idx, line in enumerate(lines):
     else:
         #print(f'idx={idx}')
         board.insert(row, [int(num) for num in line.split() if num.isdigit()])
+        marks.insert(row, [0 for num in line.split() if num.isdigit()])
         row += 1
         if row == 5:
             row = 0
             boards.append(board)
+            markboards.append(marks)
             board = [ ]
+            marks = [ ]
             skip = 1
 
 print(boards[0])
 print(boards[1])
+print(markboards[0])
+
+for number in input:
+    for bidx, board in enumerate(boards):
+        markedBoard = marknumber(number, board, markboards[bidx])
+        markboards.insert(bidx, markedBoard)
+
+print(markboards[0])
+
