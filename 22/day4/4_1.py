347a5da841d524cd7ba7ab9306eef93c40fe18d7
pfyhr <pontus@fyhr.nu>
2022-12-14 13:41:22 +0100

    famous last words indeed, python you sonofanimplicit bitch :)

--- 22/day4/4_1.py
+++ 22/day4/4_1.py
@@ -8,15 +8,17 @@ def splitline(line):
     n2list = splitline[1].split('-')
     sizen1 = listsize(n1list)
     sizen2 = listsize(n2list)
+
     #returns in conditionals... ajajj
-    if sizen1 > sizen2:
+    if sizen1 >= sizen2:
         return n1list, n2list
     else:
         return n2list, n1list
 
 def contains(large, small):
     contains = 0
-    if large[0] <= small[0] and large[1] >= small[1]:
+    if (int(large[0]) <= int(small[0])) and \
+        (int(large[1]) >= int(small[1])):
         contains = 1
     #else nothing
     return contains
@@ -26,7 +28,8 @@ with open('day4/input.txt') as file:
     lines = file.readlines()
 
     for line in lines:
-        #split line in 2, find largest set, see if it covers the smaller one
+        #split line in 2, find largest set, 
+        #see if it covers the smaller one
         large, small = splitline(line.strip())
         containpairs += contains(large, small)
 
