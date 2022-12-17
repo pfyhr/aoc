6afd61099afda4a003e13aac94edbe205a241039
pfyhr <pontus@fyhr.nu>
2022-12-17 20:08:57 +0100

    ok list is done, now for the real task.

--- 22/day5/5_1.py
+++ 22/day5/5_1.py
@@ -1,17 +1,23 @@
+from more_itertools import locate
+
 with open('day5/input.txt') as file:
     lines = file.readlines()
 
     instructions = [ [] for _ in range(9) ]
     for line in reversed(lines[0:8]):
         split = line.strip().split(' ')
+
         counter = 0
-        for idx, item in enumerate(split):
-            if item == '' and counter < 6:
-                split.pop(idx)
-                counter +=1
-            else:
-                counter = 0
-        print(split)
+        while len(split)>9:
+            for idx, item in enumerate(split):
+                if idx < 8:
+                    if split[idx] == '' and split[idx+1] == '':
+                        split.pop(idx)
+            
+        
+        # not nicely block printed... 
+        # and way more work than it should have been
+        print(split) 
         for idx, thing in enumerate(split):
             if thing != '':
                 #print(idx)
